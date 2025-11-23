a00d3d60c58263bb1a5c4246a28c1b9c12a07f0b
devprove-openedx <openedx@devprove.com>
2025-11-23 05:26:03 +0200

    Update proctoring.py

--- cms/djangoapps/contentstore/rest_api/v1/serializers/proctoring.py
+++ cms/djangoapps/contentstore/rest_api/v1/serializers/proctoring.py
@@ -13,6 +13,7 @@ class ProctoredExamSettingsSerializer(serializers.Serializer):
     enable_proctored_exams = serializers.BooleanField()
     allow_proctoring_opt_out = serializers.BooleanField()
     proctoring_provider = serializers.CharField()
+    proctoring_event_number = serializers.CharField(required=False, allow_null=True)
     proctoring_escalation_email = serializers.CharField(required=False, allow_null=True)
     create_zendesk_tickets = serializers.BooleanField()
 
@@ -21,6 +22,7 @@ class LimitedProctoredExamSettingsSerializer(serializers.Serializer):
     """Serializer for non edX Staff for proctored exam settings"""
     enable_proctored_exams = serializers.BooleanField()
     proctoring_provider = serializers.CharField()
+    proctoring_event_number = serializers.CharField(required=False, allow_null=True)
     proctoring_escalation_email = serializers.CharField(allow_blank=True)
     create_zendesk_tickets = serializers.BooleanField()
 
